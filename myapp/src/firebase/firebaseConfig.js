4040d96990b90c536fcdf3afd08088e67d9d59ed
Rishik Sinha <rishiksinha@Rishiks-MacBook-Air.local>
2024-08-11 22:53:17 +0530

    don

--- myapp/src/firebase/firebaseConfig.js
+++ myapp/src/firebase/firebaseConfig.js
@@ -1,16 +1,21 @@
 import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/auth'
+import { getAuth } from "firebase/auth";
 
+// Firebase configuration
 const firebaseConfig = {
-apiKey: "AIzaSyAq3JZWgkncuqWq_WOaYlOMZNcpdRF20VY",
-  authDomain: "authentication-9a971.firebaseapp.com",
-  projectId: "authentication-9a971",
-  storageBucket: "authentication-9a971.appspot.com",
-  messagingSenderId: "833673968152",
-  appId: "1:833673968152:web:0ddf64dd07ea3af45c216d"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
+console.log("Firebase API Key:", process.env.REACT_APP_FIREBASE_API_KEY);
 
-
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth=getAuth(app)
-export default app
+
+// Initialize Firebase Authentication
+export const auth = getAuth(app);
+
+export default app;
